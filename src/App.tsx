2841d641220a38114c6aa00d0a7f4b08f0a4190c
kelpikz <ajithaprasad96@gmail.com>
2021-10-19 19:16:09 +0530

    Init(dependency): add react router dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
+import React from "react";
 import "./styles/output.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { HomePage } from "./pages/home";
 
 function App() {
 	return (
-		<div className="bg-gray-900 p-20 h-screen flex justify-center items-start flex-col">
-			<h1 className="text-5xl text-white">Welcome to Utopia 👋</h1>
-			<p className="text-gray-400 mt-5 text-lg">
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
-				consequuntur odio aut nobis ab quis? Reiciendis doloremque ut quo fugiat
-				eveniet tempora, atque alias earum ullam inventore itaque sapiente iste?
-			</p>
-			<button className="p-4 bg-green-600 rounded-lg font-bold text-white mt-5 hover:bg-gray-600">
-				Start the journey
-			</button>
+		<div className="bg-gray-900 h-full">
+			<Router>
+				<Switch>
+					<Route path="/" exact>
+						<HomePage />
+					</Route>
+					<Route path="/game">
+						<h1>Game Route</h1>
+					</Route>
+				</Switch>
+			</Router>
 		</div>
 	);
 }
---
+++ src/components/home.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const HomePage = () => {
+	return (
+		<>
+			<div className="p-20 h-screen flex justify-center items-start flex-col">
+				<h1 className="text-5xl text-white">Welcome to Utopia 👋</h1>
+				<p className="text-gray-400 mt-5 text-lg">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+					consequuntur odio aut nobis ab quis? Reiciendis doloremque ut quo
+					fugiat eveniet tempora, atque alias earum ullam inventore itaque
+					sapiente iste?
+				</p>
+				<button className="p-4 bg-green-600 rounded-lg font-bold text-white mt-5 hover:bg-gray-600">
+					<Link to="/game">Start the journey</Link>
+				</button>
+			</div>
+		</>
+	);
+};
+
+export { HomePage };
