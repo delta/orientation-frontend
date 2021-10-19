5a7bb2852d225995850636f721767791fed88fd8
kelpikz <ajithaprasad96@gmail.com>
2021-10-19 15:52:43 +0530

    Init(boilterplate): Init project with TailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./styles/output.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="bg-gray-900 p-20 h-screen flex justify-center items-start flex-col">
+			<h1 className="text-5xl text-white">Welcome to Utopia 👋</h1>
+			<p className="text-gray-400 mt-5 text-lg">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+				consequuntur odio aut nobis ab quis? Reiciendis doloremque ut quo fugiat
+				eveniet tempora, atque alias earum ullam inventore itaque sapiente iste?
+			</p>
+			<button className="p-4 bg-green-600 rounded-lg font-bold text-white mt-5 hover:bg-gray-600">
+				Start the journey
+			</button>
+		</div>
+	);
 }
 
 export default App;
