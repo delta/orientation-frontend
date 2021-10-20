c145c503a2b596e984a17d6fdfb2080e7815409c
kelpikz <ajithaprasad96@gmail.com>
2021-10-20 19:36:17 +0530

    feat(asset-loader): create a class for Map Game Object

--- src/components/game/assetLoader.tsx
+++ src/components/game/assetLoader.tsx
@@ -1,14 +1,20 @@
 import React, { useContext, useState } from "react";
 import { loadAsset, AssetType } from "../../core/mapAssetLoader";
 import { mapData } from "../../data/map";
+import { GameMap, GameMapProps } from "../../core/MapObject";
+
 interface Props {
 	children?: React.ReactNode;
 }
 
-const fetchAllData = () => {
+/**
+ * pass an array of req Data Obj, and the caller is guaranteed to get a promise returning an
+ * array of all the assets passed as a HTML Element.
+ */
+const fetchAssetData = (reqMapData: typeof mapData) => {
 	return new Promise<AssetType[]>((resolve, reject) => {
 		const promiseArray: Promise<AssetType>[] = [];
-		mapData.forEach((singleMapData) =>
+		reqMapData.forEach((singleMapData) =>
 			promiseArray.push(loadAsset(singleMapData.source))
 		);
 		Promise.all(promiseArray)
@@ -18,6 +24,28 @@ const fetchAllData = () => {
 				reject();
 			});
 	});
+};
+
+const createDataObjects = async () => {
+	// generating map data
+	//
+	// returns an array of given asset data
+	// TODO: Make it lazy load the other map segments
+	// implement something similar to Intersection Observe API for canvas
+	// https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+	//
+	//
+
+	try {
+		const mapElements = await fetchAssetData(mapData);
+		const mapObjects = mapData.map((m, index) => {
+			return new GameMap({ ...m, imageObject: mapElements[index], zoom: 1 });
+		});
+
+		return { mapObjects };
+	} catch (err) {
+		console.error(err);
+	}
 };
 
 const AssetLoader = ({}: Props) => {
---
+++ src/core/MapObject.ts
@@ -0,0 +1,45 @@
+const MAP_ZOOM = 1;
+
+export interface GameMapProps {
+	zoom: number;
+	startX: number;
+	startY: number;
+	height: number;
+	width: number;
+	grid: number[][];
+	imageObject: HTMLImageElement | null;
+}
+
+export interface GameMapMethods extends GameMapProps {
+	// get the next map
+}
+
+export interface GameMapType extends GameMapMethods {}
+
+export class GameMap implements GameMapType {
+	zoom = MAP_ZOOM;
+	startX = 0;
+	startY = 0;
+	height = 400;
+	width = 400;
+	grid = new Array(Array<number>()); // fuck typescript
+	imageObject!: HTMLImageElement | null;
+	constructor({
+		height,
+		width,
+		imageObject,
+		startX,
+		startY,
+		grid,
+	}: GameMapProps) {
+		this.height = height;
+		this.width = width;
+		this.startX = startX;
+		this.startY = startY;
+		this.grid = grid;
+
+		if (imageObject == null) console.error("Image not loaded properly");
+
+		this.imageObject = imageObject;
+	}
+}
