2e8c1deb3bf85ec075c0051159c589b5775d9ccc
siva2204 <sivaraman26r@gmail.com>
2021-11-28 12:21:42 +0530

    [Add] build actions and pre-commit hook

--- src/config/config.example.ts
+++ src/config/config.example.ts
@@ -1,7 +1,7 @@
-export const configExample = {
-	backendOrigin: "http://localhost:8000",
-	dauth: {
-		clientId: "",
-		redirectURI: "/auth-callback.html",
-	},
+export const config = {
+  backendOrigin: "http://localhost:8000",
+  dauth: {
+    clientId: "",
+    redirectURI: "/auth-callback.html",
+  },
 };
