f415ff4cd0614ff83d0200735e053f825c8bbf0c
kelpikz <ajithaprasad96@gmail.com>
2021-12-07 05:07:08 +0530

    feat(phaser-scene): create a index file for scene dir

---
+++ src/Phaser/Scene/index.ts
@@ -0,0 +1,8 @@
+import { Scene } from './Scene';
+export { Scene };
+
+import { SceneContext } from './sceneContext';
+export { SceneContext };
+
+import { PhaserScene } from './SceneWrapper';
+export { PhaserScene };
