199df42b468f67762478e7b02dad1cfcdee74928
kelpikz <ajithaprasad96@gmail.com>
2021-12-07 02:41:59 +0530

    fix(phaser-scene): rename Scene.ts to SceneWrapper.ts

---
+++ src/Phaser/Scene/SceneWrapper.ts
@@ -0,0 +1,35 @@
+import { Scene, Types } from 'phaser';
+
+// A extension of Phaser scene which includes the preload, init and
+// create method which isn't part of default Phaser Scene
+//
+export interface IPhaserScene extends Scene {
+    /**
+     * Preload method of Phaser Scene
+     */
+    preload?: () => void;
+    /**
+     * Init method of a Phaser Scene, read phaser docs for more info
+     * @param {any} data any data you wish to pass to the init function
+     */
+    init?: (data: any) => void;
+    /**
+     * Create method of a Phaser Scene, read phaser docs for more info
+     * @param {any} data any data you wish to pass to the create function
+     */
+    create?: (data: any) => void;
+}
+
+export class PhaserScene extends Scene implements IPhaserScene {
+    preload?: () => void;
+    init?: (data: any) => void;
+    create?: (data: any) => void;
+    constructor(config: string | Types.Scenes.SettingsConfig) {
+        // right now we are not adding the newPhaserScene methods
+        // user is responsible for adding the methods on their own
+        //
+        // TODO: get the methods in constructor call itself
+        //
+        super(config);
+    }
+}
