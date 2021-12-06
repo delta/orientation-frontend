f01b5428a04cfcf172a5f9034d29d675eea6efe7
kelpikz <ajithaprasad96@gmail.com>
2021-12-07 03:27:55 +0530

    feat(phaser-scene): remove interface in phaserWrapper

--- src/Phaser/Scene/SceneWrapper.ts
+++ src/Phaser/Scene/SceneWrapper.ts
@@ -3,27 +3,21 @@ import { Scene, Types } from 'phaser';
 // A extension of Phaser scene which includes the preload, init and
 // create method which isn't part of default Phaser Scene
 //
-export interface IPhaserScene extends Scene {
+export class PhaserScene extends Scene {
     /**
      * Preload method of Phaser Scene
      */
-    preload?: () => void;
+    preload?: (() => void) | null;
     /**
      * Init method of a Phaser Scene, read phaser docs for more info
      * @param {any} data any data you wish to pass to the init function
      */
-    init?: (data: any) => void;
+    init?: ((data?: any) => void) | null;
     /**
      * Create method of a Phaser Scene, read phaser docs for more info
      * @param {any} data any data you wish to pass to the create function
      */
-    create?: (data: any) => void;
-}
-
-export class PhaserScene extends Scene implements IPhaserScene {
-    preload?: () => void;
-    init?: (data: any) => void;
-    create?: (data: any) => void;
+    create?: ((data?: any) => void) | null;
     constructor(config: string | Types.Scenes.SettingsConfig) {
         // right now we are not adding the newPhaserScene methods
         // user is responsible for adding the methods on their own
