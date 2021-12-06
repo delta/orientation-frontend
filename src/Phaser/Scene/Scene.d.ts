9c9b35f3fe923f7c4c0bff16cffc82e94bfc41e5
kelpikz <ajithaprasad96@gmail.com>
2021-12-07 02:21:27 +0530

    docs(phaser-scene): addd docs for phaser scene

--- src/Phaser/Scene/Scene.d.ts
+++ src/Phaser/Scene/Scene.d.ts
@@ -1,6 +1,21 @@
 import { Scene } from 'phaser';
+
+// A extension of Phaser scene which includes the preload, init and
+// create method which isn't part of default Phaser Scene
+//
 export interface PhaserScene extends Scene {
+    /**
+     * Preload method of Phaser Scene
+     */
     preload?: () => void;
-    init?: () => void;
-    create?: () => void;
+    /**
+     * Init method of a Phaser Scene, read phaser docs for more info
+     * @param {any} data any data you wish to pass to the init function
+     */
+    init?: (data: any) => void;
+    /**
+     * Create method of a Phaser Scene, read phaser docs for more info
+     * @param {any} data any data you wish to pass to the create function
+     */
+    create?: (data: any) => void;
 }
