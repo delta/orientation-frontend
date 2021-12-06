11ec4b9b4e8c572137bed3747228d2c3812ab225
kelpikz <ajithaprasad96@gmail.com>
2021-12-07 02:43:53 +0530

    feat(phaser-scene): add dummy init, create and preload functions

--- src/Phaser/Scene/Scene.tsx
+++ src/Phaser/Scene/Scene.tsx
@@ -1,15 +1,37 @@
 import React, { useContext, useEffect } from 'react';
+// import { GameObjects } from 'phaser';
 
+import { PhaserScene } from './SceneWrapper';
 import { GameContext } from '../Game/GameContext';
 
 const Scene = (props: any) => {
     const game = useContext(GameContext);
-    useEffect(() => console.log(game), [game]);
-    return (
-        <>
-            <div className="h-1/2 w-1/2"></div>
-        </>
-    );
+
+    function init(this: PhaserScene) {
+        console.log('init');
+    }
+
+    function preload(this: PhaserScene) {
+        console.log('preload');
+    }
+
+    function create(this: PhaserScene) {
+        console.log('create');
+    }
+    useEffect(() => {
+        if (!game) return;
+        const newScene = new PhaserScene({ key: 'firstScene' });
+        console.log(game);
+        newScene.init = init;
+        newScene.preload = preload;
+        newScene.create = create;
+        game?.scene.add('firstScene', newScene, true);
+
+        return () => {
+            game?.scene.remove('firstScene');
+        };
+    }, [game]);
+    return null;
 };
 
 export { Scene };
