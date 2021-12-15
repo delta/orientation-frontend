fb6c02df22e696073cf7eb77462a25bf7da25441
kelpikz <ajithaprasad96@gmail.com>
2021-12-15 09:25:17 +0530

    feat(gameObject): create a new function to update GameObject

---
+++ src/Phaser/GameObjects/elements/updateGameObject.ts
@@ -0,0 +1,33 @@
+import { Types, GameObjects } from 'phaser';
+import { GameElements } from './types';
+
+const TextMethodWrapper = (type: 'x' | 'y' | 'text' | 'style') => {
+    if (type === 'x')
+        return (gameObj: GameObjects.Text, val: number) => {
+            gameObj.setX(val);
+        };
+    else if (type === 'y')
+        return (gameObj: GameObjects.Text, val: number) => {
+            gameObj.setY(val);
+        };
+    else if (type === 'text')
+        return (gameObj: GameObjects.Text, val: string | string[]) => {
+            gameObj.setText(val);
+        };
+    else
+        return (
+            gameObj: GameObjects.Text,
+            val: Types.GameObjects.Text.TextStyle
+        ) => {
+            gameObj.setStyle(val);
+        };
+};
+
+export const updateData: Record<
+    GameElements.AllowedGameObjectTypes,
+    (type: any) => (...args: any) => any
+> = {
+    TEXT: TextMethodWrapper,
+    Image: TextMethodWrapper,
+    TileMap: TextMethodWrapper
+};
