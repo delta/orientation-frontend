c6a46e318a73378f5eb945ecd4507a3bc83b73a6
Raghul Nanth <nanth.raghul@gmail.com>
2021-12-21 21:28:58 +0530

    feat: Dummy login function

--- src/config/config.example.ts
+++ src/config/config.example.ts
@@ -3,6 +3,7 @@ export const config = {
     assetUrl: 'http://localhost:3000',
     websocketUrl: 'ws://localhost:8000/api/ws',
     tickRate: 2,
+    isDev: true,
     dauth: {
         clientId: '',
         redirectURI: 'http://localhost:3000/auth-callback.html'
