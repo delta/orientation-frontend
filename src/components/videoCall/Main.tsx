af5969efdc3c39a0ee9a428f0e856af57013ab95
Raghul Nanth <nanth.raghul@gmail.com>
2022-01-01 12:37:03 +0530

    feat: Added party system to videocall

--- src/components/videoCall/Main.tsx
+++ src/components/videoCall/Main.tsx
@@ -1,30 +1,26 @@
-import React from 'react';
 import { LiveKitRoom } from './LiveKitRoom';
-import { useEffect, useState, useCallback } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useState, useCallback } from 'react';
 import 'react-aspect-ratio/aspect-ratio.css';
 import styles from './styles.module.css';
 import { config } from '../../config/config';
 import { axiosInstance } from '../../utils/axios';
 function Main() {
     const [, updateToken] = useState(''); //Access token
-    const [videoComp, updateVc] = useState(<></>);
     const url = config.livekitUrl; //Livekit URL
-    const history = useHistory();
-    const [showVideo, setShowVideo] = useState(true);
-
-    const queueUser = useCallback(() => {
-        //console.log('People in room',size)
-        //TODO: Queueing system
-        //Currently just redirecting to "/game"
-    }, []);
-    useEffect(() => {
-        (async () => {
-            const connectVc = async () => {
-                //Requesting server for an access token. Server SDK generates 1 and this is passed to the livekit server
-                let resp: any = await axiosInstance.get('/api/joinvc');
-                updateToken(resp.data.token);
-                let comp = (
+    const [showVideo, setShowVideo] = useState(false);
+    const [roomName, setRoomName] = useState('');
+    const connectOldVc = async (name: string) => {
+        let resp: any = await axiosInstance.get(`/api/joinvc?room=${name}`);
+        console.log('Name:', name);
+        updateToken(resp.data.token);
+        setShowVideo(true);
+        let comp = (
+            <>
+                <h1>
+                    Your room is {resp.data.roomName}. Share it with your
+                    friends to join
+                </h1>
+                <div className="roomContainer">
                     <div className={styles.cont}>
                         <LiveKitRoom
                             url={url}
@@ -33,14 +29,62 @@ function Main() {
                             onConnected={(room) => onConnected(room)}
                         ></LiveKitRoom>
                     </div>
-                );
-                updateVc(comp);
-            };
-            await connectVc();
-        })();
-    }, []);
+                </div>
+            </>
+        );
+        updateVc(comp);
+    };
+    const [videoComp, updateVc] = useState(<></>);
 
-    return <div className="roomContainer">{videoComp}</div>;
+    const queueUser = useCallback(() => {
+        //console.log('People in room',size)
+        //TODO: Queueing system
+        //Currently just redirecting to "/game"
+    }, []);
+    const connectVc = async () => {
+        //Requesting server for an access token. Server SDK generates 1 and this is passed to the livekit server
+        let resp: any = await axiosInstance.get('/api/joinvc');
+        console.log(resp.data);
+        updateToken(resp.data.token);
+        setShowVideo(true);
+        let comp = (
+            <>
+                <h1>
+                    Your room is {resp.data.roomName}. Share it with your
+                    friends to join
+                </h1>
+                <div className="roomContainer">
+                    <div className={styles.cont}>
+                        <LiveKitRoom
+                            url={url}
+                            token={resp.data.token}
+                            queuefunc={queueUser}
+                            onConnected={(room) => onConnected(room)}
+                        ></LiveKitRoom>
+                    </div>
+                </div>
+            </>
+        );
+        updateVc(comp);
+    };
+    if (showVideo && videoComp != <></>) {
+        return videoComp;
+    } else
+        return (
+            <>
+                <input
+                    onChange={(e) => {
+                        console.log(e.target.value);
+                        setRoomName(e.target.value);
+                    }}
+                />
+                <button onClick={() => connectOldVc(roomName)}>Submit</button>
+                <br />
+                <button onClick={() => connectVc()}>
+                    Click to create a new room
+                </button>
+            </>
+        );
 }
 async function onConnected(room: any) {
     await room.localParticipant.setCameraEnabled(false);
