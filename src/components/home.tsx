710a2a27ebce15796a5f09bc1541800dd3474417
kelpikz <ajithaprasad96@gmail.com>
2022-01-02 02:59:24 +0530

    feat: add proper text in landing page

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -10,10 +10,8 @@ const HomePage = () => {
             <div className="p-20 h-screen flex justify-center items-start flex-col">
                 <h1 className="text-5xl text-text">Welcome to Utopia 👋</h1>
                 <p className="text-gray-400 mt-5 text-lg">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Fugit consequuntur odio aut nobis ab quis? Reiciendis
-                    doloremque ut quo fugiat eveniet tempora, atque alias earum
-                    ullam inventore itaque sapiente iste?
+                    Lets go on a journey where all that matters is the friends
+                    we make along the way!
                 </p>
                 <Link
                     to={
