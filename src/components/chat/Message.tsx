d0af149ce15120845a723519b974ba203208dcf1
Marmik Upadhyay <marmikupadhyay.mu@gmail.com>
2022-01-03 01:10:00 +0530

    fix(styling): auto scroll, word wrap, code style

--- src/components/chat/Message.tsx
+++ src/components/chat/Message.tsx
@@ -4,7 +4,17 @@ import { getColor } from './utils/colors';
 
 const Message: React.FC<{ message: IMessage }> = ({ message }) => {
     return (
-        <div className="py-2 text-lg">
+        <div className="py-2 text-lg"
+        style={
+            {
+                wordWrap: 'break-word',
+                whiteSpace: 'pre-wrap',
+                wordBreak: 'break-word',
+                overflowWrap: 'break-word',
+                overflowX: 'hidden',
+            }                
+        }
+        >
             <span className={`${getColor(message.id)} font-semibold pr-2`}>
                 <span className="underline capitalize">
                     {message.from.name}
