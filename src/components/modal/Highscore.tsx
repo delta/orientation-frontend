fc081dfa727a1f414ded5cfc86f1e80986198606
kelpikz <ajithaprasad96@gmail.com>
2022-01-04 15:32:53 +0530

    fix: fix highscore avatar spriteMap

--- src/components/modal/Highscore.tsx
+++ src/components/modal/Highscore.tsx
@@ -15,7 +15,7 @@ interface highScoreProps {
 
 const spriteIdMap: Record<string, string> = {
     player: '/favicon.ico',
-    male1: '/sprites/preview/male.png',
+    male: '/sprites/preview/male.png',
     male2: '/sprites/preview/male2.png',
     female: '/sprites/preview/female.png',
     female2: '/sprites/preview/female2.png'
